742d3b167884a131bc8e5e8958a3fb2567452b4b
Naitik Kheni <kheninaitik7@gmail.com>
2023-04-06 22:49:22 +0530

    Create Sample.js

---
+++ Sample.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello TRUEBooking</h1>
+      <h2>Start editing to see Hotels!</h2>
+    </div>
+  );
+}
